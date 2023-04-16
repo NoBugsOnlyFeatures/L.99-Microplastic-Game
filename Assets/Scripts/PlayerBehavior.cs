c8325cd526084c128ca5116a52f81b4ae3eada7c
OnlyFeatures <soren.j.ludwig@gmail.com>
2023-04-16 14:17:41 -0700

    Adjustments to swimming feel, added upper bounds to test collision

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -5,20 +5,18 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerBehavior : MonoBehaviour
 {
-    float underWaterDrag = 3f;
-    float underwaterAngularDrag = 1f;
-    Vector2 swimDirection;
-    float swimForce = 10f;
-    float waterHeight = 0f;
+    [SerializeField] float underWaterDrag = 2f;
+    [SerializeField] float underwaterAngularDrag = 1f;
+    [SerializeField] float swimForce = 150f;
     Rigidbody2D _rb;
-    bool underwater;
-    // Start is called before the first frame update
+    Vector2 swimDirection;
+
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
 
-        // _rb.drag = underWaterDrag;
-        // _rb.angularDrag = underwaterAngularDrag;
+        _rb.drag = underWaterDrag;
+        _rb.angularDrag = underwaterAngularDrag;
     }
 
     void Update()
@@ -36,89 +34,15 @@ public class PlayerBehavior : MonoBehaviour
         swimDirection = Vector2.zero;
         if (Input.GetKey(KeyCode.S))
         {
-            Debug.Log("S");
             swimDirection += Vector2.down;
         } else if (Input.GetKey(KeyCode.W)){
             swimDirection += Vector2.up;
-            Debug.Log("W");
         } 
  
         if (Input.GetKey(KeyCode.D)){
             swimDirection += Vector2.right;
-            Debug.Log("D");
         } else if (Input.GetKey(KeyCode.A)){
             swimDirection += Vector2.left;
-            Debug.Log("A");
         }
     }
-
-    //     if (Input.GetKey(KeyCode.S))
-    //     {
-    //         if (Input.GetKey(KeyCode.A))
-    //         {
-    //             transform.eulerAngles = new Vector3(
-    //                 transform.eulerAngles.x,
-    //                 transform.eulerAngles.y,
-    //                 225);
-
-    //         }
-    //         else if (Input.GetKey(KeyCode.D))
-    //         {
-    //             transform.eulerAngles = new Vector3(
-    //             transform.eulerAngles.x,
-    //             transform.eulerAngles.y,
-    //             -45);
-    //         }
-    //         else
-    //         {
-    //             transform.eulerAngles = new Vector3(
-    //             transform.eulerAngles.x,
-    //             transform.eulerAngles.y,
-    //             -90);
-    //         }
-
-    //     }
-    //     else if (Input.GetKey(KeyCode.W))
-    //     {
-    //         if (Input.GetKey(KeyCode.A))
-    //         {
-    //             transform.eulerAngles = new Vector3(
-    //             transform.eulerAngles.x,
-    //             transform.eulerAngles.y,
-    //             135);
-
-    //         }
-    //         else if (Input.GetKey(KeyCode.D))
-    //         {
-    //             transform.eulerAngles = new Vector3(
-    //             transform.eulerAngles.x,
-    //             transform.eulerAngles.y,
-    //             45);
-    //         }
-    //         else
-    //         {
-    //             transform.eulerAngles = new Vector3(
-    //             transform.eulerAngles.x,
-    //             transform.eulerAngles.y,
-    //             90);
-    //         }
-    //     }
-    //     else
-    //     {
-    //         if (Input.GetKey(KeyCode.D))
-    //         {
-    //             transform.eulerAngles = new Vector3(
-    //                 transform.eulerAngles.x,
-    //                 transform.eulerAngles.y,
-    //                 0);
-    //         }
-    //         else if (Input.GetKey(KeyCode.A))
-    //         {
-    //             transform.eulerAngles = new Vector3(
-    //                 transform.eulerAngles.x,
-    //                 transform.eulerAngles.y,
-    //                 180);
-    //         }
-    //     }
-    // }
 }
